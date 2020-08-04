b4e996b709bdf4af912aa7da4024263a34e15b56
marcuskmlee <z5164880@ad.unsw.edu.au>
2020-08-04 21:44:06 +1000

    Begun segmentation

--- project.py
+++ project.py
@@ -78,41 +78,89 @@ def minNeighbourhood(N,image,x,y):
 	# cv2.waitKey(0)
 	return neighbourhood.min()
 
-def ContrastStretch(image, N):
+def ContrastStretch(image, N, M):
 	(h, w) = image.shape
 	A = image.copy()
 	B = image.copy()
-	for x in range(0,h):
-		for y in range(0,w):
-			A[x][y] = maxNeighbourhood(N,I,x,y)
+	# for x in range(0,h):
+	# 	for y in range(0,w):
+	# 		A[x][y] = maxNeighbourhood(N,I,x,y)
 
-	for x in range(0,h):
-		for y in range(0,w):
-			B[x][y] = minNeighbourhood(N,A,x,y)
+	# for x in range(0,h):
+	# 	for y in range(0,w):
+	# 		B[x][y] = minNeighbourhood(N,A,x,y)
+
+	# #Invert the colours so that the image comes out with a white background similar to the original
+	# O = ~(cv2.subtract(B,I))
+	# return O
+	if M == 0:
+	#If M == 0 then we perform max-filtering, then min-filtering
+		for x in range(0,h):
+			for y in range(0,w):
+				A[x][y] = maxNeighbourhood(N,I,x,y)
+
+		for x in range(0,h):
+			for y in range(0,w):
+				B[x][y] = minNeighbourhood(N,A,x,y)
+		cv2.imshow("A",A)
+		cv2.imshow("B",B)
+		cv2.waitKey()
 
 	#Invert the colours so that the image comes out with a white background similar to the original
-	O = ~(cv2.subtract(B,I))
-	return O
+		O = (cv2.subtract(B,I))
 
-def DetectCells(image):
+	elif M == 1:
+	#If m == 1 then we perform min-filtering, then max-filtering
+		for x in range(0,h):
+			for y in range(0,w):
+				A[x][y] = minNeighbourhood(N,I,x,y)
+
+		for x in range(0,h):
+			for y in range(0,w):
+				B[x][y] = maxNeighbourhood(N,A,x,y)
+
+		cv2.imshow("A",A)
+		cv2.imshow("B",B)
+		cv2.waitKey()
+		O = ~(cv2.subtract(I,B))
+
+	return O
+def ColourCells(image):
 	(h,w,d) = image.shape
 	# kernel = np.ones((5,5),np.float32)/25
 	# dst = cv2.bilateralFilter(image,-1,15,10)
-	
-	# return dst
+	# blur = cv2.GaussianBlur(image,(2,2),0)
+	# return blur
+	cellCount = 0
+	for y in range(0,h):
+		for x in range(0,w):
+			if(image[y][x][0] > 10):
+				image[y][x][2] = 255-cellCount
+				cellCount += 1
+
+# def findTop(image, y, x):
+
+
+				
+
 	
 
-I = cv2.imread("COMP9517 20T2 Group Project Image Sequences/DIC-C2DH-HeLa/Sequence 1/t000.tif", cv2.IMREAD_GRAYSCALE)	
-CS = ContrastStretch(I,45)
-# kernel = np.ones((40,40), np.uint(8))
-# TH = cv2.morphologyEx(I, cv2.MORPH_TOPHAT, kernel)
-# cv2.imshow("I",I)
+I = cv2.imread("Data/FLUO-N2DL-HeLa/Sequence 1/t000.tif", cv2.IMREAD_GRAYSCALE)	
+# CS = ContrastStretch(I,30,0)
+_ ,Blackbg = cv2.threshold(I,129,255,cv2.THRESH_TOZERO)
+# BlackGauss = cv2.GaussianBlur(Blackbg,(9,9),0)
+BlackOpen = cv2.morphologyEx(Blackbg, cv2.MORPH_OPEN, (3,3))
+toCells = cv2.cvtColor(BlackOpen, cv2.COLOR_GRAY2BGR)
+ColourCells(toCells)
+cv2.imshow("I",I)
 # cv2.imshow("CS", CS)
+cv2.imshow("black", BlackOpen)
+cv2.imshow("Cells", toCells)
 # cv2.imshow("colorCS", cv2.cvtColor(CS, cv2.COLOR_GRAY2BGR))
 # cv2.waitKey()
-Cells = DetectCells(cv2.cvtColor(CS, cv2.COLOR_GRAY2BGR))
+# Cells = DetectCells(cv2.cvtColor(CS, cv2.COLOR_GRAY2BGR))
 # cv2.imshow("Cells", CS)
-# cv2.waitKey()
+cv2.waitKey()
 
 
 
